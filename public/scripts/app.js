613c0ece435c8dea3b16de536c2a923a87ac97fd
Arnold <arnoldjchang@gmail.com>
2017-09-14 22:50:58 -0700

    Indecision App First Commit

---
+++ public/scripts/app.js
@@ -0,0 +1,7 @@
+console.log('App.js is running!');
+
+// JSX - Javascript XML
+var template = <p>This is JSX from app.js!</p>;
+var appRoot = document.getElementById(app);
+
+ReactDOM.render(template, appRoot);
